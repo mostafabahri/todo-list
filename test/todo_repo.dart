aa04fc446e40577233f8b2fd1ff97cbdfab1f8fa
Mostafa Bahri <mostafabahri.dev@gmail.com>
2019-12-30 14:28:34 +0330

    add test for repo

---
+++ test/todo_repo.dart
@@ -0,0 +1,35 @@
+import 'package:colors/todo.dart';
+import 'package:colors/todo_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Parses json to correct value and type', () {
+    var jsonString = '''
+    [{"text": "first", "completed": true, "time": "2018-05-30"},
+    {"text": "second todo", "completed": false, "time": "2019-12-30 00:41:13"}]''';
+    var todos = TodosJsonDecoder().decode(jsonString);
+
+    var expected = [
+      Todo(
+        text: 'first',
+        completed: true,
+        time: DateTime.parse('2018-05-30'),
+      ),
+      Todo(
+        text: 'second todo',
+        completed: false,
+        time: DateTime.parse('2019-12-30 00:41:13'),
+      )
+    ];
+    expect(todos, equals(expected));
+    expect(todos, TypeMatcher<List<Todo>>());
+  });
+
+  test('Parses initial null value correctly', () {
+    var jsonString;
+
+    var todos = TodosJsonDecoder().decode(jsonString);
+    expect(todos, TypeMatcher<List<Todo>>());
+    expect(todos, equals(List<Todo>()));
+  });
+}
